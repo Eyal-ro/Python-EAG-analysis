fa96726050f2b308699728367169e2405262ced3
yifatweiss <81528665+yifatweiss@users.noreply.github.com>
2021-07-01 12:06:20 +0300

    added test for slice button

--- tests_EAGGui.py
+++ tests_EAGGui.py
@@ -1,1 +1,21 @@
+def test_slice_button_is_disabled():
+    loadedData = EAGanalysis("Raw data - mix and segments, 12.5.21.ASC")
+
+    #main loop
+    test_main = Tk()
+    AnalysisTimeFrame = Entry(test_main)
+    AnalysisTimeFrame.insert(0, "1")
+    SlicedData = Button(text="Slice data")
+
+    #checked if slice data disabled
+    slice_data(AnalysisTimeFrame,SlicedData,loadedData)
+    if SlicedData["state"] == DISABLED:
+        test_main.destroy()
+        return "success test_slice_button_is_disabled"
+    else:
+        test_main.destroy()
+        return "error test_slice_button_is_disabled"
+
+
+    test_main.mainloop()
 
